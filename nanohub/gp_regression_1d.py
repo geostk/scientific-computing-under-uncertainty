4e05acf07f02df9cbfc7c77bdea9991ef2d663d6
Ilias Bilionis <ebilionis@gmail.com>
2014-11-25 22:08:55 -0500

    Finished.

--- nanohub/gp_regression_1d.py
+++ nanohub/gp_regression_1d.py
@@ -13,6 +13,8 @@ Date:
 
 import GPy
 import numpy as np
+from optparse import OptionParser
+import cPickle as pickle
 
 
 def _check_and_regularize_1d_array(x):
@@ -63,10 +65,10 @@ def run(x, y,
                                                 (1D `np.ndarray` of size x_eval.shape[0])
                                 + y_var:        the predictive variance
                                                 (1D `np.ndarray` of size x_eval.shape[0])
-                                + y_q05:        the 5% lower quantile of the predictive
+                                + y_025:        the 2.5% lower quantile of the predictive
                                                 distribution of the GP
                                                 (1D `np.ndarray` of size x_eval.shape[0])
-                                + y_q95:        the 5% lower quantile of the predictive
+                                + y_975:        the 97.5% lower quantile of the predictive
                                                 distribution of the GP
                                                 (1D `np.ndarray` of size x_eval.shape[0])
                                 + y_s:          samples from the predictive distribution
@@ -92,7 +94,9 @@ def run(x, y,
     # Initialize the kernel
     k = GPy.kern.RBF(1, lengthscale=length_scale, variance=variance)
     # Initialize the mod
-    model = GPy.models.GPRegression(x, y, kernel=k, normalizer=True)
+    model = GPy.models.GPRegression(x, y, kernel=k,
+                                    normalizer=True)
+    model.Gaussian_noise.variance = noise_variance
     print '=' * 80
     print '1D Gaussian Process Regression Demo'.center(80)
     print 'Author:'
@@ -111,14 +115,69 @@ def run(x, y,
     print str(model)
     print '-' * 80
     if optimize:
-        model.optimize()
+        print 'Optimizing'.center(80)
+        print '-' * 80
+        model.optimize(optimizer='lbfgs', messages=True)
+        print '-' * 80
         print 'Optimized Model'.center(80)
         print '-' * 80
         print str(model)
         print '-' * 80
+    y_mu, y_var = model.predict(x_eval)
+    y_025, y_975 = model.predict_quantiles(x_eval)
+    return locals()
+
+
+
 
 if __name__ == '__main__':
-    data = np.loadtxt('data/Rat43.dat')
+    parser = OptionParser()
+    parser.add_option('-i', '--input', dest='input_file',
+                      default='test.dat',
+                      help='the input file (TXT with two columns)')
+    parser.add_option('-l', '--length-scale', dest='length_scale',
+                      default=1.,
+                      help='the length scale of the SE kernel')
+    parser.add_option('-s', '--signal-strength', dest='signal_strength',
+                      default=1.,
+                      help='the signal strength of the SE kernel')
+    parser.add_option('-n', '--noise-variance', dest='noise_variance',
+                      default=0.1,
+                      help='the noise variance added to the SE kernel')
+    parser.add_option('--optimize', action='store_true', dest='optimize',
+                      default=False,
+                      help='optimize the marginal likelihood or not')
+    parser.add_option('-o', '--output', dest='output_file',
+                      default=None,
+                      help='specify the output file')
+    parser.add_option('--plot', action='store_true', dest='plot',
+                      default=False,
+                      help='plot the results')
+    parser.add_option('--fig-file', dest='fig_file',
+                      default=None,
+                      help='specify the png file')
+    options, args = parser.parse_args()
+    data = np.loadtxt(options.input_file)
     x = data[:, 0]
     y = data[:, 1]
-    out = run(x, y, optimize=True)
+    out = run(x, y, optimize=options.optimize,
+              length_scale=options.length_scale,
+              variance=options.signal_strength,
+              noise_variance=options.noise_variance)
+    if options.output_file is None:
+        options.output_file = options.input_file + '.out'
+    out_data = np.hstack([out['x_eval'], out['y_mu'], out['y_025'], out['y_975']])
+    np.savetxt(options.output_file, out_data)
+    trained_model_file = options.input_file + '.pcl'
+    with open(trained_model_file, 'wb') as fd:
+        pickle.dump(out['model'], fd, protocol=pickle.HIGHEST_PROTOCOL)
+    if options.plot:
+        import matplotlib.pyplot as plt
+        plt.plot(x, y, 'r+', markersize=10, markeredgewidth=2)
+        plt.plot(out['x_eval'], out['y_mu'], 'b', linewidth=2, label='mean')
+        plt.fill_between(out['x_eval'].flatten(), out['y_025'].flatten(),
+                         out['y_975'].flatten(), color='grey', alpha=0.5,
+                         label='predictive interval')
+        if options.fig_file is None:
+            options.fig_file = options.input_file + '.png'
+        plt.savefig(options.fig_file)
